b426ad6727e76484c273fce70bdf1dc31e18aad1
NikitaMane12 <manenikita2212@gmail.com>
2024-07-23 19:50:22 +0530

    teting

--- Frontend/Backend/index.js
+++ Frontend/Backend/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const connectToDB = require("./src/configs/db");
 const { userRouter } = require("./src/routes/user.routes");
 const { eventRouter } = require("./src/routes/event.routes");
@@ -9,18 +9,18 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use("/users",userRouter)
-app.use("/events",eventRouter)
+app.use("/users", userRouter);
+app.use("/events", eventRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, async() => {
+app.listen(port, async () => {
   try {
-    await connectToDB(process.env.mongo_URI)
+    await connectToDB(process.env.MONGO_URI);
     console.log(`Server is running at ${port}`);
   } catch (error) {
-   console.log(error); 
+    console.log(error);
   }
 });
